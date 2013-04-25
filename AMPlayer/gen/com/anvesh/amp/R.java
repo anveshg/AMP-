e967a6bdc42008412ffe6f5e89896e6c02034d3a
anveshg <anvesh.g90@gmail.com>
2013-04-25 17:02:02 +0530

    V1 test

--- AMPlayer/gen/com/anvesh/amp/R.java
+++ AMPlayer/gen/com/anvesh/amp/R.java
@@ -24,15 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
+        public static final int action_settings=0x7f090007;
         public static final int button1=0x7f090001;
         public static final int button2=0x7f090000;
         public static final int button3=0x7f090002;
-        public static final int button4=0x7f090004;
-        public static final int button5=0x7f090005;
-        public static final int seekBar1=0x7f090003;
+        public static final int button4=0x7f090003;
+        public static final int button5=0x7f090004;
+        public static final int button6=0x7f090005;
         public static final int textView1=0x7f090006;
-        public static final int textView2=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- AMPlayer/src/com/anvesh/amp/MainActivity.java
+++ AMPlayer/src/com/anvesh/amp/MainActivity.java
@@ -1,91 +1,142 @@
 package com.anvesh.amp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	int elt,tot;
 	Button button;
 	MediaPlayer musicplayer;
-	TextView textview;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+//Inits
 		final Button playButton = (Button) findViewById(R.id.button1);
 		final Button pauseButton = (Button) findViewById(R.id.button2);
 		final Button stopButton = (Button) findViewById(R.id.button3);
 		final Button ffButton = (Button) findViewById(R.id.button5);
 		final Button rewButton = (Button) findViewById(R.id.button4);
-		final TextView elapsd = (TextView) findViewById(R.id.textView1);
-		final TextView totl = (TextView) findViewById(R.id.textView2);
-
+		final Button weatherButton = (Button) findViewById(R.id.button6);
 		musicplayer = MediaPlayer.create(getApplicationContext(), R.raw.livey);
-		elt= musicplayer.getCurrentPosition();
-		tot=musicplayer.getDuration();
+	
+	
+//Location 
+		//reference to the system   location manager  
+		LocationManager locatonManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);  
+		Location location;
+		double latitude; // latitude
+	    double longitude; // longitude
 
-		playButton.setOnClickListener(new OnClickListener() {
+ 
+		LocationListener locationListener = new LocationListener() {  
 
-			@Override
-			public void onClick(View vone) {
-				Toast.makeText(getApplicationContext(), "Playing", Toast.LENGTH_LONG).show();
-				musicplayer.start();
-			}
-		});
+			@Override  
+			public void onStatusChanged(String provider, int status, Bundle extras) {  
 
-		pauseButton.setOnClickListener(new OnClickListener() {
+				//Toast.makeText(getApplicationContext(), "provider status changed", Toast.LENGTH_LONG).show();  
 
-			@Override
-			public void onClick(View vtwo) {
-				musicplayer.pause();
-				Toast.makeText(getApplicationContext(), "Pausing", Toast.LENGTH_LONG).show();
-			}
-		});
+			}  
 
-		stopButton.setOnClickListener(new OnClickListener() {
+			@Override  
+			public void onProviderEnabled(String provider) {  
 
-			@Override
-			public void onClick(View vthree) {
-				musicplayer.stop();
-				Toast.makeText(getApplicationContext(), "Stopping", Toast.LENGTH_LONG).show();
-				musicplayer.prepareAsync();
-				musicplayer.seekTo(0);
+				//Toast.makeText(getApplicationContext(), "Provider enabled", Toast.LENGTH_LONG).show();  
 
-			}
-		});
+			}  
 
-		rewButton.setOnClickListener(new OnClickListener() {
+			@Override  
+			public void onProviderDisabled(String provider) {  
 
-			@Override
-			public void onClick(View vfour) {
-				if(musicplayer.isPlaying())
-				{
-					musicplayer.seekTo(musicplayer.getCurrentPosition() - 5000);
+				//Toast.makeText(getApplicationContext(), "Provider disabled", Toast.LENGTH_LONG).show();  
+
+			}  
+
+			@Override  
+			public void onLocationChanged(Location location) {  
+
+				//Toast.makeText(getApplicationContext(), "New Location", Toast.LENGTH_LONG).show();  
+			}};
+
+
+			weatherButton.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View vone) {
+
 				}
+			});
+			//Register location manager with the listener  
+			locatonManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);  
+			//locatonManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);  
 
-			}
-		});
 
-		ffButton.setOnClickListener(new OnClickListener() {
+//Media Player
+			playButton.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View vfive) {
-				if(musicplayer.isPlaying())
-				{
-					musicplayer.seekTo(musicplayer.getCurrentPosition() + 5000);
+				@Override
+				public void onClick(View vone) {
+					Toast.makeText(getApplicationContext(), "Playing", Toast.LENGTH_LONG).show();
+					musicplayer.start();
 				}
+			});
 
-			}
-		});
+			pauseButton.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View vtwo) {
+					musicplayer.pause();
+					Toast.makeText(getApplicationContext(), "Pausing", Toast.LENGTH_LONG).show();
+				}
+			});
+
+			stopButton.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View vthree) {
+					musicplayer.stop();
+					Toast.makeText(getApplicationContext(), "Stopping", Toast.LENGTH_LONG).show();
+					musicplayer.prepareAsync();
+					musicplayer.seekTo(0);
+
+				}
+			});
+
+			rewButton.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View vfour) {
+					if(musicplayer.isPlaying())
+					{
+						musicplayer.seekTo(musicplayer.getCurrentPosition() - 5000);
+					}
+
+				}
+			});
+
+			ffButton.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View vfive) {
+					if(musicplayer.isPlaying())
+					{
+						musicplayer.seekTo(musicplayer.getCurrentPosition() + 5000);
+					}
+
+				}
+			});
 
 	}
 
